e5cbe5db769161138663f4e4707fd0044bde4a36
Pengkui Luo <pengkui.luo@gmail.com>
2013-06-12 14:22:44 -0700

    SingletonInit

--- singleton.py
+++ singleton.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+http://code.activestate.com/recipes/102187-singleton-as-a-metaclass/
+http://stackoverflow.com/questions/674304/pythons-use-of-new-and-init
+http://docs.python.org/reference/datamodel.html#basic-customization
+
+"""
 
 __all__ = [
     'Singleton',
+    'SingletonInit',
 ]
 
 class Singleton (type):
-    """Singleton metaclass
+    """ Singleton metaclass.
+        If an instance of the class exists, just return that instance.
+    """
+    _instances = {}
+    def __call__ (cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton,cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-    http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
-    http://code.activestate.com/recipes/102187-singleton-as-a-metaclass/
-    http://stackoverflow.com/questions/674304/pythons-use-of-new-and-init
-    http://docs.python.org/reference/datamodel.html#basic-customization
+
+class SingletonInit (type):
+    """ Singleton metaclass.
+        If an instance of the class exists, *run the __init__()* for this
+        instance, and then return the instance.
     """
     _instances = {}
     def __call__ (cls, *args, **kwargs):
@@ -20,4 +36,3 @@ class Singleton (type):
         else:  # run __init__ every time the class is called
             cls._instances[cls].__init__(*args, **kwargs)
         return cls._instances[cls]
-
