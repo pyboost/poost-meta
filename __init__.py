eb96e7c7d86ae8032ad3bd901a113661c73b6a00
Pengkui Luo <pengkui.luo@gmail.com>
2013-07-19 21:29:46 -0700

    --- Switched to year-based versioning system, e.g. 0.13.1-a1.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Pengkui Luo <pengkui.luo@gmail.com>
+# Created 06/11/2013, updated 07/19/2013
+#
+"""
+"""
+from __future__ import absolute_import
+
+print('Executing %s' %  __file__)
+
+import sys
+if not (2, 6) <= sys.version_info < (3, ):
+    raise ImportError("CPython 2.6.x or 2.7.x is required (%d.%d detected)."
+                      % sys.version_info[:2])
 
 from .singleton import *
 
-__version__ = '0.1.0-a1'
+del sys, absolute_import
+
+__version__ = '0.13.1-a1'
